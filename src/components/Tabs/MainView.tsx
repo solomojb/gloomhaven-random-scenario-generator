1f97ca0c7d0683b894309cddede71f7355618546
Jim Solomon <jsolomon@vistaprint.com>
2021-07-09 14:54:22 -0400

    Removed Hex Grid Example

--- src/components/Tabs/MainView.tsx
+++ src/components/Tabs/MainView.tsx
@@ -4,7 +4,6 @@ import FlagsProvider, { ShowFlags } from '../Providers/FlagsProvider';
 import ScenarioProvider from '../Providers/ScenarioProvider';
 import MapContainer from './Maps/MapContainer';
 import ScenarioContainer from './Scenario/ScenarioContainer';
-import {ExampleContainer} from './Example/ExampleContainer';
 
 const MainView = () => {
     const [ currentTab, setCurrentTab] = useState<number>(parseFloat((localStorage.getItem("lastTab") as string|| "0")));
@@ -22,7 +21,6 @@ const MainView = () => {
             </FlagsProvider>
         </Tab.Pane> },
         { menuItem: 'Map', render: () => <Tab.Pane><MapContainer/></Tab.Pane> },
-        { menuItem: 'HexGrid2', render: () => <Tab.Pane><ExampleContainer/></Tab.Pane> },
     ];
 
 
